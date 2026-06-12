47e9c7183883a66c6abd291500abab025906117a
scenicmediaproductions-web <scenicmediaproductions@gmail.com>
2026-06-13 02:14:58 +0530

    Website fully working

--- script.js
+++ script.js
@@ -84,3 +84,21 @@ window.addEventListener("scroll", () => {
     });
 
 });
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll",()=>{
+
+reveals.forEach((element)=>{
+
+let windowHeight=window.innerHeight;
+let top=element.getBoundingClientRect().top;
+
+if(top<windowHeight-100){
+
+element.classList.add("active");
+
+}
+
+});
+
+});
